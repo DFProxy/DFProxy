6b03dd639b48c775867a71a2ec72e48e52ec7f2c
SiebeDW <de.wit.siebe@gmail.com>
2019-09-24 19:54:57 +0200

    messed up but fixed

--- src/clientpackets/chat.js
+++ src/clientpackets/chat.js
@@ -1,30 +1,27 @@
-<<<<<<< HEAD
 const PacketEvent = require('../structures/packetevent.js')
-=======
+
 //
 // The client CHAT class is a packet class
-// that handles in-game Minecraft chat routed 
+// that handles in-game Minecraft chat routed
 // through the client.
 //
-const Packet = require('../structures/packet.js');
->>>>>>> 57666f6466cc5b8ba7709fbdf527d0ea30b8f131
 
 module.exports = class Chat extends PacketEvent {
   constructor (client) {
     super(client, {
       name: 'chat'
-    });
+    })
   }
 
   run (meta, data, client, proxyClient, proxy) {
-    var message = data.message;
+    var message = data.message
     if (message.startsWith('/')) {
-      const args = message.split(/\s+/g);
-      const command = args.shift().substr(1);
-      const cmd = this.dfproxy.commands.get(command);
+      const args = message.split(/\s+/g)
+      const command = args.shift().substr(1)
+      const cmd = this.dfproxy.commands.get(command)
       if (cmd && cmd.run) {
-        this.cancelPacket();
-        cmd.run();
+        this.cancelPacket()
+        cmd.run()
       }
     }
   }
