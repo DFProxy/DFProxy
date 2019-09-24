67b98115be9eff745418546c279167dba8c8c60d
LittleWhole <wholelittle@gmail.com>
2019-09-24 08:52:54 +0800

    Update login.js

--- src/clientpackets/login.js
+++ src/clientpackets/login.js
@@ -1,14 +1,19 @@
-const Packet = require('../structures/packet.js')
+//
+// The LOGIN class is a packet class
+// that handles the event when a player
+// logs in to DiamondFire.
+//
+const Packet = require('../structures/packet.js');
 
 module.exports = class Login extends Packet {
   constructor (client) {
     super(client, {
       name: 'login'
-    })
+    });
   }
 
   run (meta, data, client, proxyClient, proxy) {
-    console.log('hi')
-    this.announce('Welcome Back!')
+    console.log('hi');
+    this.announce('Welcome Back!');
   }
 }
