57666f6466cc5b8ba7709fbdf527d0ea30b8f131
LittleWhole <wholelittle@gmail.com>
2019-09-24 08:56:23 +0800

    Class description and Siebe's lack of semicolons

--- src/serverpackets/chat.js
+++ src/serverpackets/chat.js
@@ -1,10 +1,15 @@
-const Packet = require('../structures/packet.js')
+//
+// The server-side CHAT class is a
+// packet class that handles in-game
+// chat on the server side.
+//
+const Packet = require('../structures/packet.js');
 
 module.exports = class Chat extends Packet {
   constructor (client) {
     super(client, {
       name: 'chat'
-    })
+    });
   }
 
   async run (meta, data, client, proxyClient, proxy) {
