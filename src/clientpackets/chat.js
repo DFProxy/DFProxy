d1eea0e57e7032bfa515575b624c0fc90f800c28
LittleWhole <wholelittle@gmail.com>
2019-09-24 08:49:39 +0800

    Update chat.js

--- src/clientpackets/chat.js
+++ src/clientpackets/chat.js
@@ -1,21 +1,26 @@
+//
+// The CHAT class is a packet class
+// that handles in-game Minecraft
+// chat.
+//
 const Packet = require('../structures/packet.js')
 
 module.exports = class Chat extends Packet {
   constructor (client) {
     super(client, {
       name: 'chat'
-    })
+    });
   }
 
   run (meta, data, client, proxyClient, proxy) {
-    var message = data.message
+    var message = data.message;
     if (message.startsWith('/')) {
-      const args = message.split(/\s+/g)
-      const command = args.shift().substr(1)
-      const cmd = this.dfproxy.commands.get(command)
+      const args = message.split(/\s+/g);
+      const command = args.shift().substr(1);
+      const cmd = this.dfproxy.commands.get(command);
       if (cmd && cmd.run) {
-        this.cancelPacket()
-        cmd.run()
+        this.cancelPacket();
+        cmd.run();
       }
     }
   }
