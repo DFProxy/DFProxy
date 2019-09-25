737fe2ea7dd0f070f0463cbadeb56ec73313c63d
SiebeDW <de.wit.siebe@gmail.com>
2019-09-25 17:28:23 +0200

    config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const DFProxy = require('./structures/dfproxy.js')
-var dfproxy = new DFProxy({ port: 25566, email: 'smack--snack@hotmail.com', password: 'santsnack1995' })
+const config = require('./config.json')
+let dfproxy
+if (!config.email || !config.password || !config.port) {
+  dfproxy = new DFProxy({ port: 25565, email: 'smack--snack@hotmail.com', password: 'santsnack1995' })
+} else {
+  dfproxy = new DFProxy({ port: config.port, email: config.email, password: config.password })
+}
+
 dfproxy.loadCommands('../commands/')
 dfproxy.loadServerPacketsEvents('../serverpackets/')
 dfproxy.loadClientPacketsEvents('../clientpackets/')
