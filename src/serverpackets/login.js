ff75f5b0e5fd80e9315c13f7f1bcfc54b68d09e9
SiebeDW <de.wit.siebe@gmail.com>
2019-09-25 19:29:09 +0200

    A lot of usefull things + some testing

---
+++ src/serverpackets/login.js
@@ -0,0 +1,18 @@
+const PacketEvent = require('../structures/packetevent.js')
+
+module.exports = class Login extends PacketEvent {
+  constructor (client) {
+    super(client, {
+      name: 'login'
+    })
+  }
+
+  async run (meta, data, client, proxyClient, proxy) {
+    client.entityId = data.entityId
+    client.gamemode = data.gameMode
+    client.dimension = data.dimension
+    client.difficulty = data.difficulty
+    client.maxPlayers = data.maxPlayers
+    client.levelType = data.levelType
+  }
+}
