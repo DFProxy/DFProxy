e7dd77b92c1a296c037663498810edcf1deac789
SiebeDW <de.wit.siebe@gmail.com>
2019-09-27 20:30:29 +0200

    Inventory support :D

--- src/serverpackets/set_slot.js
+++ src/serverpackets/set_slot.js
@@ -8,9 +8,7 @@ module.exports = class SetSlot extends PacketEvent {
   }
 
   async run (meta, data, client, proxyClient, proxy) {
-    // Does not work, needs fix!
     client.inventory.updateSlot(data.slot, this.dfproxy.Item.fromNotch(data.item))
-    console.log(client.inventory.items())
-    console.log('Updated inventory slot')
+    // Update Inventory
   }
 }
