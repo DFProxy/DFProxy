fcb6790a4d098ea6bf1b34bfb12558f50bbcf821
SiebeDW <de.wit.siebe@gmail.com>
2019-09-28 16:19:28 +0200

    semistandard, travis ci stuff

--- src/serverpackets/respawn.js
+++ src/serverpackets/respawn.js
@@ -1,15 +1,15 @@
-const PacketEvent = require('../structures/packetevent.js')
+const PacketEvent = require('../structures/packetevent.js');
 
 module.exports = class Respawn extends PacketEvent {
   constructor (client) {
     super(client, {
       name: 'respawn'
-    })
+    });
   }
 
   async run (meta, data, client, proxyClient, proxy) {
-    client.dimension = data.dimension
-    client.difficulty = data.difficulty
-    client.gamemode = data.gamemode
+    client.dimension = data.dimension;
+    client.difficulty = data.difficulty;
+    client.gamemode = data.gamemode;
   }
-}
+};
