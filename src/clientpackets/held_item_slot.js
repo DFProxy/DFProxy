18d3ec71a425bb037bc17d7dd031da19c1a32413
SiebeDW <de.wit.siebe@gmail.com>
2019-09-29 15:04:59 +0200

    work

---
+++ src/clientpackets/held_item_slot.js
@@ -0,0 +1,13 @@
+const PacketEvent = require('../structures/packetevent.js');
+
+module.exports = class HeldItemSlot extends PacketEvent {
+  constructor (client) {
+    super(client, {
+      name: 'held_item_slot'
+    });
+  }
+
+  async run (meta, data, client, proxyClient, proxy) {
+    client.slot = data.slotId;
+  }
+};
