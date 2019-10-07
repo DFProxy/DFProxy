168512d5ced7790bb700efde4bbe2171111aed4b
Reasonless <tylerwalker05@hotmail.co.uk>
2019-10-07 18:10:24 +0100

    First Commit
    Removed the spammy crap and added chat to packetevent.js

--- src/structures/packetevent.js
+++ src/structures/packetevent.js
@@ -1,13 +1,14 @@
 class PacketEvent {
   constructor (dfproxy, options) {
-    /**
-             * Spammy :)
-             * @type {Spammy}
-             */
     this.dfproxy = dfproxy;
 
     this.name = options.name; // Packet name
     this.canceled = false;
+  }
+
+  chat (text) {
+    if (!this.dfproxy.client) return;
+    this.dfproxy.client.write('chat', { message: `{"extra":[{"text":"${text}"}],"text":""}`, position: 1 });
   }
 
   cancelPacket () {
