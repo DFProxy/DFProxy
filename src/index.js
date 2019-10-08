5232bc3a2d88084250aed2db9a9e65c9e75fe65e
SiebeDW <de.wit.siebe@gmail.com>
2019-10-08 18:53:25 +0200

    new index

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const DFProxy = require('./structures/dfproxy.js');
+const config = require('./config.json');
+let dfproxy;
+
+if (!process.env.CI) {
+  if (!config.email || !config.password || !config.port) {
+    console.log('There is no username/password/port in the config! Exiting in 15 seconds...');
+    process.exit(1);
+  } else {
+    dfproxy = new DFProxy({ port: config.port, email: config.email, password: config.password });
+  }
+} else {
+  dfproxy = new DFProxy({ port: config.port, email: process.env.email, password: process.env.password });
+}
+
+dfproxy.loadCommands('../commands/', () => {
+  dfproxy.loadServerPacketsEvents('../serverpackets/', () => {
+    dfproxy.loadClientPacketsEvents('../clientpackets/', () => {
+      dfproxy.loadCustomActions('../customactions', () => {
+        if (process.env.CI) {
+          console.log('Done loading! Seems like we are in a CI. Exiting with code 0.');
+          process.exit(0);
+        } else {
+          console.log('Done loading!');
+        }
+      });
+    });
+  });
+});
+
+process
+  .on('uncaughtException', err => error(err.stack))
+  .on('unhandledRejection', err => error(err.stack));
+function error (err) {
+  if (process.env.CI) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('ERROR!\n' + err);
+  if (dfproxy.client) {
+    dfproxy.client.end('§cWhooops! Error found :(!\nRejoin to play again!\n§c§lPlease report the error (logged in console) in our discord!');
+    dfproxy.client = undefined;
+    dfproxy.proxyClient = undefined;
+  }
+}
